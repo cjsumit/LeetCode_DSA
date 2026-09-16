8ea96f3132cf81e2a81f15c83795e01223cf1754
Sumit Vishwakarma <168545778+cjsumit@users.noreply.github.com>
2026-09-16 15:24:09 +0530

    Time: 1 ms (11.02%) | Memory: 46.4 MB (61.90%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int idx = nums.length;
+        int[] arr = new int[idx];
+        int i=0,j=n;
+        while(i<idx && j<idx){
+            if(i%2==0){
+                arr[i] = nums[i/2];
+            }else {
+                arr[i] = nums[j];
+                j++;
+            }
+            i++;
+        }
+        return arr;
+    }
+}
