73c6fa73d68a5313b01ed96df64561c2cfb728a2
Sumit Vishwakarma <168545778+cjsumit@users.noreply.github.com>
2026-09-22 19:30:03 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (32.47%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,19 +1,28 @@
 class Solution {
     public int missingNumber(int[] arr) {
-        int n =arr.length;
-        int i =0;
-        while(i<arr.length){
-            if(arr[i]==i || arr[i]==n) i++;
-            else{
-                int idx = arr[i];
-                int temp = arr[idx];
-                arr[idx] = arr[i];
-                arr[i] = temp;
-            }
+        // int n =arr.length;
+        // int i =0;
+        // while(i<arr.length){
+        //     if(arr[i]==i || arr[i]==n) i++;
+        //     else{
+        //         int idx = arr[i];
+        //         int temp = arr[idx];
+        //         arr[idx] = arr[i];
+        //         arr[i] = temp;
+        //     }
+        // }
+        // for(i=0;i<arr.length;i++){
+        //     if(arr[i] != i) return i;
+        // }
+        // return n;
+
+
+        int xor=0;
+        xor=xor^arr.length;
+        for(int i=0;i<arr.length;i++){
+            xor=xor^i^arr[i];
         }
-        for(i=0;i<arr.length;i++){
-            if(arr[i] != i) return i;
-        }
-        return n;
+        return xor;
+
     }
 }
