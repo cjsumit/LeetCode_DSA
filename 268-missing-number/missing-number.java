d42681fd7e3e97312d1ec79d0fa9996d1de9d7d9
Sumit Vishwakarma <168545778+cjsumit@users.noreply.github.com>
2026-09-23 10:49:13 +0530

    Time: 1 ms (28.81%) | Memory: 46.9 MB (95.21%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,28 +1,30 @@
 class Solution {
     public int missingNumber(int[] arr) {
-        // int n =arr.length;
-        // int i =0;
-        // while(i<arr.length){
-        //     if(arr[i]==i || arr[i]==n) i++;
-        //     else{
-        //         int idx = arr[i];
-        //         int temp = arr[idx];
-        //         arr[idx] = arr[i];
-        //         arr[i] = temp;
-        //     }
-        // }
-        // for(i=0;i<arr.length;i++){
-        //     if(arr[i] != i) return i;
-        // }
-        // return n;
-
-
-        int xor=0;
-        xor=xor^arr.length;
-        for(int i=0;i<arr.length;i++){
-            xor=xor^i^arr[i];
+        // Using Cyclic Sorting Algorithm!!!
+        int n =arr.length;
+        int i =0;
+        while(i<arr.length){
+            if(arr[i]==i || arr[i]==n) i++;
+            else{
+                int idx = arr[i];
+                int temp = arr[idx];
+                arr[idx] = arr[i];
+                arr[i] = temp;
+            }
         }
-        return xor;
+        for(i=0;i<arr.length;i++){
+            if(arr[i] != i) return i;
+        }
+        return n;
+
+        //Best Approach using XOR!!
+
+        // int xor=0;
+        // xor=xor^arr.length;
+        // for(int i=0;i<arr.length;i++){
+        //     xor=xor^i^arr[i];
+        // }
+        // return xor;
 
     }
 }
